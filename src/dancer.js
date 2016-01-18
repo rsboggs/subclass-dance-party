1be930bfc755a342669443dcd26d30f30134d79c
rsboggs <robertstephenboggs@gmail.com>
2016-01-18 15:28:22 -0800

    Renamed Classes

--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,5 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
+var Dancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
@@ -9,13 +9,13 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.setPosition(top, left);
 };
 
-makeDancer.prototype.step = function() {
+Dancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   setTimeout((function() {this.step();}).bind(this), this.timeBetweenSteps);
 };
 
-makeDancer.prototype.setPosition = function(top, left) {
+Dancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
